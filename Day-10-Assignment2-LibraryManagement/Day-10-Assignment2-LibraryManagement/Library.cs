bd6aa9175667ff8d846650c5aed49bf0e3dda2b0
Aditya Raj <1611adityaraj@gmail.com>
2026-04-29 14:53:00 +0530

    Added Library Management Assignment

---
+++ Day-10-Assignment2-LibraryManagement/Day-10-Assignment2-LibraryManagement/Library.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace LibraryManagement
+
+{
+    public class Library
+    {
+        public List<Book> Books { get; set; } = new List<Book>();
+        public List<Borrower> Borrowers { get; set; } = new List<Borrower>();
+
+        public void AddBook(Book book)
+        {
+            Books.Add(book);
+        }
+
+        public void RegisterBorrower(Borrower borrower)
+        {
+            Borrowers.Add(borrower);
+        }
+
+        public void BorrowBook(string isbn, string cardNumber)
+        {
+            var book = Books.FirstOrDefault(b => b.ISBN == isbn);
+            var borrower = Borrowers.FirstOrDefault(b => b.LibraryCardNumber == cardNumber);
+
+            if (book != null && borrower != null && !book.IsBorrowed)
+            {
+                book.IsBorrowed = true;
+                borrower.BorrowedBooks.Add(book);
+            }
+        }
+
+        public void ReturnBook(string isbn, string cardNumber)
+        {
+            var book = Books.FirstOrDefault(b => b.ISBN == isbn);
+            var borrower = Borrowers.FirstOrDefault(b => b.LibraryCardNumber == cardNumber);
+
+            if (book != null && borrower != null && book.IsBorrowed)
+            {
+                book.IsBorrowed = false;
+                borrower.BorrowedBooks.Remove(book);
+            }
+        }
+
+        public List<Book> ViewBooks()
+        {
+            return Books;
+        }
+
+        public List<Borrower> ViewBorrowers()
+        {
+            return Borrowers;
+        }
+    }
+
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string ISBN { get; set; }
+        public bool IsBorrowed { get; set; }
+    }
+
+    public class Borrower
+    {
+        public string Name { get; set; }
+        public string LibraryCardNumber { get; set; }
+
+        public List<Book> BorrowedBooks { get; set; } = new List<Book>();
+    }
+
+}
+    
+
+
+
+        
+    
---
+++ Day-10-Assignment2-LibraryManagement/LibraryManagementTests/Test1.cs
@@ -0,0 +1,90 @@
+﻿using LibraryManagement;
+//using Day-10-Assignment2-LibraryManagement;
+//using Microsoft.Extensions.DependencyModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+
+
+
+namespace LibraryManagementTests
+{
+    [TestClass]
+    public class Test1
+    {
+        //
+        //private Microsoft.Extensions.DependencyModel.Library library = null!;
+        private Library library = null;
+
+        [TestInitialize]
+        public void Setup()
+        {
+           // library = new Microsoft.Extensions.DependencyModel.Library();
+           library = new Library();
+
+        }
+
+        [TestMethod]
+        public void Test_Add_Book()
+        {
+            var book = new Book { Title = "C#", Author = "MS", ISBN = "123" };
+            library.AddBook(book);
+
+            Assert.AreEqual(1, library.Books.Count);
+        }
+
+        [TestMethod]
+        public void Test_Register_Borrower()
+        {
+            var borrower = new Borrower { Name = "Aditya", LibraryCardNumber = "001" };
+            library.RegisterBorrower(borrower);
+
+            Assert.AreEqual(1, library.Borrowers.Count);
+        }
+
+        [TestMethod]
+        public void Test_Borrow_Book()
+        {
+            var book = new Book { Title = "C#", Author = "MS", ISBN = "123" };
+            var borrower = new Borrower { Name = "Aditya", LibraryCardNumber = "001" };
+
+            library.AddBook(book);
+            library.RegisterBorrower(borrower);
+
+            library.BorrowBook("123", "001");
+
+            Assert.IsTrue(book.IsBorrowed);
+            Assert.AreEqual(1, borrower.BorrowedBooks.Count);
+        }
+
+        [TestMethod]
+        public void Test_Return_Book()
+        {
+            var book = new Book { Title = "C#", Author = "MS", ISBN = "123" };
+            var borrower = new Borrower { Name = "Aditya", LibraryCardNumber = "001" };
+
+            library.AddBook(book);
+            library.RegisterBorrower(borrower);
+
+            library.BorrowBook("123", "001");
+            library.ReturnBook("123", "001");
+
+            Assert.IsFalse(book.IsBorrowed);
+            Assert.AreEqual(0, borrower.BorrowedBooks.Count);
+        }
+
+        [TestMethod]
+        public void Test_View()
+        {
+            var book = new Book { Title = "C#", Author = "MS", ISBN = "123" };
+            var borrower = new Borrower { Name = "Aditya", LibraryCardNumber = "001" };
+
+            library.AddBook(book);
+            library.RegisterBorrower(borrower);
+
+            Assert.AreEqual(1, library.ViewBooks().Count);
+            Assert.AreEqual(1, library.ViewBorrowers().Count);
+        }
+    }
+
+}
