aa306e22ccf6a67d65e70d16343ae43d2e7fec79
Aditya Raj <146013239+adsingh409@users.noreply.github.com>
2026-05-05 11:16:27 +0530

    Add files via upload

---
+++ Day16_WebApplication1/Day16_WebApplication1/Controllers/ValuesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Day16_WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "API chal rahi hai 🚀";
+        }
+    }
+}
---
+++ Day16_WebApplication1/Day16_WebApplication1/CustomMiddleware.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+
+namespace Day16_WebApplication1
+{
+    public class CustomMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public CustomMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            Console.WriteLine("Request: " + context.Request.Path);
+
+            await _next(context);
+
+            Console.WriteLine("Response Done");
+        }
+    }
+}
