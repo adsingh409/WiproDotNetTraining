6ca76733d942a0ff7e394ed180de0411eb792c59
Aditya Raj <146013239+adsingh409@users.noreply.github.com>
2026-05-19 11:24:05 +0530

    Add files via upload

---
+++ Day21/BookStoreApp/BookStoreApp/Controllers/BooksController.cs
@@ -0,0 +1,54 @@
+﻿using BookStoreApp.Data;
+using BookStoreApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStoreApp.Controllers
+{
+    public class BooksController : Controller
+    {
+        private readonly BookRepository repo;
+
+        public BooksController(IConfiguration config)
+        {
+            repo = new BookRepository(config.GetConnectionString("DefaultConnection"));
+        }
+
+        public IActionResult Index()
+        {
+            var books = repo.GetBooks();
+            return View(books);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Book book)
+        {
+            repo.AddBook(book);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var book = repo.GetBooks().FirstOrDefault(x => x.Id == id);
+            return View(book);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Book book)
+        {
+            repo.UpdateBook(book);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            repo.DeleteBook(id);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ Day21/BookStoreApp/BookStoreApp/Data/BookRepository.cs
@@ -0,0 +1,94 @@
+﻿using BookStoreApp.Models;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace BookStoreApp.Data
+{
+    public class BookRepository
+    {
+        private readonly string connectionString;
+
+        public BookRepository(string conn)
+        {
+            connectionString = conn;
+        }
+
+        // 🔹 GET ALL BOOKS
+        public List<Book> GetBooks()
+        {
+            List<Book> list = new List<Book>();
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("SELECT * FROM Books", con);
+                con.Open();
+
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    list.Add(new Book
+                    {
+                        Id = (int)reader["Id"],
+                        Title = reader["Title"].ToString(),
+                        Author = reader["Author"].ToString(),
+                        Price = (decimal)reader["Price"]
+                    });
+                }
+            }
+            return list;
+        }
+
+        // 🔹 ADD BOOK
+        public void AddBook(Book book)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("AddBook", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@Title", book.Title);
+                cmd.Parameters.AddWithValue("@Author", book.Author);
+                cmd.Parameters.AddWithValue("@Price", book.Price);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        // 🔹 UPDATE BOOK
+        public void UpdateBook(Book book)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("UpdateBook", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@Id", book.Id);
+                cmd.Parameters.AddWithValue("@Title", book.Title);
+                cmd.Parameters.AddWithValue("@Author", book.Author);
+                cmd.Parameters.AddWithValue("@Price", book.Price);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        // 🔹 DELETE BOOK
+        public void DeleteBook(int id)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("DeleteBook", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@Id", id);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+    }
+}
